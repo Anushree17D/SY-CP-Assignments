339f5cea587eb76c3a4343b211f541323c0c4d78
Anushree Dixit <anushreedixit635@gmail.com>
2026-01-06 10:31:57 +0530

    Create Q4.cpp

---
+++ Experiment 1/Q4.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+ 
+int main(){
+    int base, power;
+    cout << "Enter the base: " <<endl; 
+    cin >> base;
+    cout << "Enter the power: " <<endl; 
+    cin >> power;
+
+    cout << "Result: " << pow(base,power) << endl; 
+ 
+    return 0;
+}
