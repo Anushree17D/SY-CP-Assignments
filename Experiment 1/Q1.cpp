6cfcbb2188c16526a0c8e263d12104670a9084eb
Anushree Dixit <anushreedixit635@gmail.com>
2026-01-06 10:04:25 +0530

    Create Q1.cpp

---
+++ Experiment 1/Q1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+#include <sstream>
+ 
+int main(){
+    string str;
+    cout << "Enter a string: " << endl;
+    getline(cin, str);
+
+
+    stringstream ss(str);
+    string wrd;
+    int c = 0;
+
+    while (ss >> wrd){
+        c++;
+    }
+
+    cout << "The number of words in the string u entered is: " << c << endl;
+ 
+    return 0;
+}
