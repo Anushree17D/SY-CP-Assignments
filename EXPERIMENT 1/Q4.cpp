29354e7e4f6350d3a8ad03d058b667bab2500381
Anushree Dixit <anushreedixit635@gmail.com>
2026-01-26 17:38:56 +0530

    Q4

---
+++ EXPERIMENT 1/Q4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int m, n;
+    cout << "Enter base: " << endl;
+    cin >> m;
+    cout << "Enter power: " << endl;
+    cin >> n;
+
+    cout << "Result: " << pow(m, n);
+    return 0;
+}
