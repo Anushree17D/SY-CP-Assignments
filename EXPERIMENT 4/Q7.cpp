56849262fb385d9bc333238ed347b638ad1bde73
Anushree Dixit <anushreedixit635@gmail.com>
2026-02-14 11:11:55 +0530

    Create Q7.cpp

---
+++ EXPERIMENT 4/Q7.cpp
@@ -0,0 +1,30 @@
+// TC :- O(n^2)
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter size of matrix: " << endl;
+    cin >> n;
+
+    vector<vector<int>> arr(n, vector<int>(n));
+
+    cout << "Enter elements of the matrix: " << endl;
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            cin >> arr[i][j];
+
+    for (int i = 0; i < n; i++) {
+        if (i % 2 == 0) {
+            for (int j = 0; j < n; j++)
+                cout << arr[i][j] << " ";
+        } else {
+            for (int j = n - 1; j >= 0; j--)
+                cout << arr[i][j] << " ";
+        }
+    }
+
+    return 0;
+}
