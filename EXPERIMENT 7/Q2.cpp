073239c851d6a131ccf45e34b0934db954dd24a3
Anushree Dixit <anushreedixit635@gmail.com>
2026-04-24 20:10:55 +0530

    Create Q2.cpp

---
+++ EXPERIMENT 7/Q2.cpp
@@ -0,0 +1,30 @@
+// TC :- O(n²)
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    int n;
+    cout << "Enter size: ";
+    cin >> n;
+
+    vector<int> arr(n);
+
+    cout << "Enter elements: ";
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    for(int i = 0; i < n-1; i++){
+        for(int j = 0; j < n-i-1; j++){
+            if(arr[j] > arr[j+1])
+                swap(arr[j], arr[j+1]); // STL swap
+        }
+    }
+
+    cout << "Sorted array: ";
+    for(int x : arr)
+        cout << x << " ";
+
+    return 0;
+}
